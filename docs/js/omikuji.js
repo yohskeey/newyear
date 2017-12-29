b5510808b11194f682d5522add222325db0741b4
Yohskeey <indecision_guy@hotmail.com>
2017-12-29 20:25:04 +0900

    リファクタリング

--- docs/js/omikuji.js
+++ docs/js/omikuji.js
@@ -1,31 +1,31 @@
 
 const omikuji = () =>{
 
-    document.getElementById("result").innerHTML = "大吉";
-    var num = Math.floor(Math.random()*100+1 );
+    let num = Math.floor(Math.random()*100+1 );
+    let resElem = document.getElementById("result");
     if(num<=5){
-        document.querySelector("#result").innerHTML = "大吉";
-        document.querySelector("#result").style.backgroundColor = "yellow";
+        resElem.innerHTML = "大吉";
+        resElem.style.backgroundColor = "yellow";
 
     }else if(num>=6 && num<=20){
-        document.querySelector("#result").innerHTML = "中吉";
-        document.querySelector("#result").style.backgroundColor = "orange";
+        resElem.innerHTML = "中吉";
+        resElem.style.backgroundColor = "orange";
 
     }else if(num>=21 && num<=50){
-        document.querySelector("#result").innerHTML = "小吉";
-        document.querySelector("#result").style.backgroundColor = "pink";
+        resElem.innerHTML = "小吉";
+        resElem.style.backgroundColor = "pink";
 
     }else if(num>=51 && num<=80){
-        document.querySelector("#result").innerHTML = "吉";
-        document.querySelector("#result").style.backgroundColor = "lightblue";
+        resElem.innerHTML = "吉";
+        resElem.style.backgroundColor = "lightblue";
 
     }else if(num>=81 && num<=99){
-        document.querySelector("#result").innerHTML = "末吉";
-        document.querySelector("#result").style.backgroundColor = "lightgreen";
+        resElem.innerHTML = "末吉";
+        resElem.style.backgroundColor = "lightgreen";
 
     }else if(num==100){
-        document.querySelector("#result").innerHTML = "凶";
-        document.querySelector("#result").style.backgroundColor = "rec";
+        resElem.innerHTML = "凶";
+        resElem.style.backgroundColor = "rec";
 
     }
     document.getElementById("image").style.visibility = "visible";
